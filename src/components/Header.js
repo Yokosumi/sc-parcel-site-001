f97a261d1402736225a15aba2f6b913cbc383028
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-10-19 15:11:06 +0200

    added components

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return /*html*/ `
+    <h1 class="text-blue-950 text-4xl">Parcel Site</h1>
+    `;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.scss";
+import { Header } from "./components/Header";
 
 document.querySelector("#app")!.innerHTML = /*html*/ `
-  <h1 class="text-blue-950 text-4xl">Parcel Site 2</h1>
-  <img src="/images/code.png">
+  ${Header()}
 `;
